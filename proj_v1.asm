85f0de3516291dc917f57fb3aae489468528013b
Arthur Maluf <102260550+ArthrM@users.noreply.github.com>
2022-11-01 18:52:10 -0300

    v1.1
    + Validação de input dos dois valores e da operação
    + Unificação dos processos de impressão para imprimir qualquer resposta com dois dígitos

--- proj_v1.asm
+++ proj_v1.asm
@@ -2,39 +2,86 @@ TITLE
 .model small
 .data
     MSG1 DB 'Primeiro valor: ','$'
-    MSG2 DB 10,'Operacao : ','$'
-    MSG3 DB 10,'Segundo valor: ','$'
+    MSG2 DB 'Operacao : ','$'
+    MSG3 DB 'Segundo valor: ','$'
     MSG4 DB 10,10,'Resultado: ','$'
+    MSG5 DB 10,'!Invalido! - ','$'
 .code
 main proc
     MOV AX,@DATA
     MOV DS,AX
 
+NUM1:
     LEA DX,MSG1
     MOV AH,09
     INT 21H
-
+    
     MOV AH,01
     INT 21H ; leitura do primeiro numero
+    MOV CH,30H
+    VALNUM1:
+        CMP AL,CH
+        JZ GOODNUM1
+        INC CH
+        CMP CH,3AH
+        JNZ VALNUM1
+
+    LEA DX,MSG5
+    MOV AH,09
+    INT 21H
+    JMP NUM1
+GOODNUM1:
     SUB AL,30H
     MOV BL,AL
-
+    MOV AH,02
+    MOV DL,10
+    INT 21H
+OPSELECT:
     LEA DX,MSG2
     MOV AH,09
     INT 21H 
 
     MOV AH,01
-    INT 21H ; leitura da operação
+    INT 21H ; leitura da operacao
 
+    CMP AL,'+'
+    JZ VALIDOP
+    CMP AL,'-'
+    JZ VALIDOP
+    CMP AL,'/'
+    JZ VALIDOP
+    CMP AL,'*'
+    JZ VALIDOP
+
+    LEA DX,MSG5
+    MOV AH,09
+    INT 21H
+    JMP OPSELECT
+VALIDOP:
     MOV CL,AL
-
+    MOV AH,02
+    MOV DL,10
+    INT 21H
+NUM2:
     LEA DX,MSG3
     MOV AH,09
     INT 21H
 
     MOV AH,01
     INT 21H ; leitura do segundo numero
+    MOV CH,30H
+    VALNUM2:
+        CMP AL,CH
+        JZ GOODNUM2
+        INC CH
+        CMP CH,3AH
+        JNZ VALNUM2
 
+    LEA DX,MSG5
+    MOV AH,09
+    INT 21H
+    JMP NUM2
+GOODNUM2:
     SUB AL,30H
     MOV BH,AL
 
@@ -46,72 +93,55 @@ main proc
     JZ DIVI
     CMP CL,'*'
     JZ MULT
+    
 
 SOMA:
     ADD BH,BL
-    CMP BH,0AH
-    JGE RESP2
-    ADD BH,30H
     JMP RESP
 SUBT:
-    CMP BL,BH
-    JB NEGA
     SUB BL,BH
-    ADD BL,30H
     MOV BH,BL
     JMP RESP
-    NEGA:
-        SUB BH,BL
-        LEA DX,MSG4
-        MOV AH,09
-        INT 21H
-        MOV DL,'-'
+DIVI:
+    
+MULT:
+    XOR CL,CL
+    VOLTAMULT:
+        TEST BH,01H
+        JE PULAMULT
+        ADD CL,BL
+        PULAMULT:
+            SHL BL,1
+            SHR BH,1
+            JNZ VOLTAMULT
+            MOV BH,CL
+            CMP CL,0AH
+            JMP RESP
+RESP:
+    LEA DX,MSG4
+    MOV AH,09
+    INT 21H
+    CMP BH,0
+    JGE POS
+    NEG BH
+    MOV AH,02
+    MOV DL,'-'
+    INT 21H
+    POS:
+        XOR AX,AX
+        MOV AL,BH
+        MOV BH,10D
+        DIV BH
+        MOV BH,AH
+        MOV BL,AL
+        MOV DL,BL
+        ADD DL,30H
         MOV AH,02
         INT 21H
         MOV DL,BH
         ADD DL,30H
         INT 21H
-        JMP FIM
-DIVI:
-    
-MULT:
-    DEC BH
-    MOV CH,BL
-    MULT2:
-        ADD BL,CH
-        DEC BH
-        JNZ MULT2
-    MOV BH,BL
-    CMP BH,0AH
-    JGE RESP2
-    ADD BH,30H
-    
-RESP:
-    LEA DX,MSG4
-    MOV AH,09
-    INT 21H
-    MOV DL,BH
-    MOV AH,02
-    INT 21H
-    JMP FIM
-RESP2:
-    XOR AX,AX
-    MOV AL,BH
-    MOV BH,10D
-    DIV BH
-    MOV BH,AH
-    MOV BL,AL
-    LEA DX,MSG4
-    MOV AH,09
-    INT 21H
-    MOV DL,BL
-    ADD DL,30H
-    MOV AH,02
-    INT 21H
-    MOV DL,BH
-    ADD DL,30H
-    INT 21H
-FIM:
+
     MOV AH,4CH
     INT 21H
 main endp
